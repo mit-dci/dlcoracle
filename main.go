dc9c4bb0f46f13ca83935eb7405e27848a177e1f
Gert-Jaap Glasbergen <gertjaap@gertjaap.nl>
2018-05-05 10:36:16 +0200

    CORS headers for accessing the oracle from LIT webUI

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"github.com/gertjaap/dlcoracle/store"
 
 	"github.com/awnumar/memguard"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -46,7 +47,12 @@ func main() {
 	r.HandleFunc("/api/publication/{R}", routes.PublicationHandler)
 	r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("static"))))
 
+	// CORS
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
 	logging.Info.Println("Listening on port 3000")
 
-	logging.Error.Fatal(http.ListenAndServe(":3000", logging.WebLoggingMiddleware(r)))
+	logging.Error.Fatal(http.ListenAndServe(":3000", handlers.CORS(originsOk, headersOk, methodsOk)(logging.WebLoggingMiddleware(r))))
 }
